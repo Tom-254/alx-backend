471b746cac233c371741217e77750b13ddbf7394
Tom-254 <211mseeratiego@gmail.com>
2023-04-25 23:35:20 +0000

    0x02. i18n

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -6,7 +6,8 @@ A basic Flask application that serves a single page.
 
 from flask import Flask, render_template
 
-app = Flask(__name__, strict_slashes=False)
+app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.route('/')
